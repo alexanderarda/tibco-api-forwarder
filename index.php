bc981c6f63bcb452474a2cc04ee1d6f7893ad155
DESKTOP-U7C37G2\alexa <alexandertyas@live.com>
2018-07-12 15:11:54 +0700

    json example

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-okay
+{
+  "transaction_time": "2018-04-17 07:50:42",
+  "transaction_status": "settlement",
+  "transaction_id": "8a46ff5a-c588-4098-a160-6e3d0680accb",
+  "status_message": "midtrans payment notification",
+  "status_code": "200",
+  "signature_key": "4e1098774daaf492a22536ddee07e836d4d5c1366b79ee3ee217891a332bcbc07b7a558df88522be3b88cb3c07ac5b5c78087b537c9f25ef41ebc9d54843d742",
+  "permata_va_number": "8778004698284919",
+  "payment_type": "bank_transfer",
+  "order_id": "041712-62-ISO.1-015-OWD-00234",
+  "gross_amount": "10001.00"
+}
