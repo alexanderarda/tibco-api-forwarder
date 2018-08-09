9e8f6b31f6a27f8c7af411b273da2764b98d4622
DESKTOP-U7C37G2\alexa <alexandertyas@gmail.com>
2018-08-09 15:51:51 +0700

    api call controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,24 +5,71 @@ namespace App\Http\Controllers;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
 
 class ApiController{
 
+    private $clientEndpoint;
+    public function __construct()
+    {
+        $this->clientEndpoint = 'http://dki.herokuapp.com';
+    }
+
     public function getBalance($accNo){
 
+
+        try {
         $client = new Client([
-            'base_uri' => 'http://dki.herokuapp.com'
+            'base_uri' => $this->clientEndpoint
         ]);
 
-        $response = $client->get('api');
+        $response = $client->get($accNo);
 
-        echo '<pre>'; dd($response);
+            return response($response->getBody())
+                ->header('Content-Type', 'application/json');
 
-        echo $accNo;
+        } catch (RequestException $e) {
+
+            if ($e->hasResponse()) {
+
+                $err =  Psr7\str($e->getResponse());
+
+                return response()->json([
+                    'message' => $err,
+                ], 404);
+
+            }
+        }
+
+
+
     }
 
     public function getHistory($accNo){
-        echo $accNo;
+
+
+        try {
+            $client = new Client([
+                'base_uri' => $this->clientEndpoint
+            ]);
+
+            $response = $client->get($accNo);
+
+            return response($response->getBody())
+                ->header('Content-Type', 'application/json');
+
+        } catch (RequestException $e) {
+            //echo Psr7\str($e->getRequest());
+            if ($e->hasResponse()) {
+                $err =  Psr7\str($e->getResponse());
+
+                return response()->json([
+                    'message' => $err,
+                ], 404);
+
+            }
+        }
     }
 
 
